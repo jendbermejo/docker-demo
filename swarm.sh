55ceac027489f6efc99d250c2a804e3c9e5f7ba5
Jen Bermejo <jdbermejo@voyagerinnovation.com>
2018-06-17 18:46:43 +0800

    update swarm script

--- swarm.sh
+++ swarm.sh
@@ -5,7 +5,7 @@ TOKEN=$(docker swarm join-token -q manager)
 
 for i in 2 ; do
   eval $(docker-machine env node$i --shell sh)
-  docker swarm join --advertise-addr $(docker-machine ip node$i) \ --token $TOKEN $(docker-machine ip node1):2377
+  docker swarm join --advertise-addr $(docker-machine ip node$i) --token $TOKEN $(docker-machine ip node1):2377
 done
 
 eval $(docker-machine env node1 --shell sh)
