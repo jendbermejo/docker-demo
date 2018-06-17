a90f0b1f2edc4b507d75e922eb3d30158f0d68e6
Jen Bermejo <jdbermejo@voyagerinnovation.com>
2018-06-17 18:40:45 +0800

    update swarm.sh

--- swarm.sh
+++ swarm.sh
@@ -1,9 +1,12 @@
 eval $(docker-machine env node1 --shell sh)
 docker swarm init --advertise-addr $(docker-machine ip node1)
+
 TOKEN=$(docker swarm join-token -q manager)
+
 for i in 2 ; do
-  eval $(docker-machine env node$i)
+  eval $(docker-machine env node$i --shell sh)
   docker swarm join --advertise-addr $(docker-machine ip node$i) \ --token $TOKEN $(docker-machine ip node1):2377
 done
-eval $(docker-machine env node1)
+
+eval $(docker-machine env node1 --shell sh)
 docker node ls
