24759115404df8e73a1619380b393c6e6951f4ac
Jen Bermejo <jdbermejo@voyagerinnovation.com>
2018-06-18 04:44:51 +0800

    update stack.sh

--- stack.sh
+++ stack.sh
@@ -1,2 +1,2 @@
 eval $(docker-machine env node1 --shell sh)
-docker stack deploy -c $JENKINS_HOME/workspace/SimpleDockerDemo/docker-compose.yml stock13
+docker stack deploy -c $JENKINS_HOME/workspace/DockerSwarmDeployment/docker-compose.yml stock13
